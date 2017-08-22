d8b8cfb451e8a82c70f0d1c46dfe9195e87a750a
Joshua <jbkunst@gmail.com>
2017-08-22 18:40:46 -0300

    structura

--- R/setup.R
+++ R/setup.R
@@ -1,13 +1,14 @@
 rm(list = ls())
 library(tidyverse)
+library(viridis)
 library(highcharter)
 library(jbkmisc)
+library(scales)
+library(stringr)
 
 knitr::opts_chunk$set(message = FALSE, warning = FALSE, echo = TRUE)
 options(highcharter.theme = hc_theme_smpl(chart = list(backgroundColor = "transparent")))
-
-# remove temporal widgets
-file.remove(setdiff(dir(pattern = ".html"), "index.html"))
+theme_set(theme_jbk(base_family = NA, plot_margin = margin(10, 10, 10, 10)))
 
 # helpers -----------------------------------------------------------------
 R <- function() {  as.character(tags$span("R", style = "color:#2066B9;font-weight:500")) }
