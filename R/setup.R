041158d090d4244abc4605db281cb27c5add63f8
Joshua Kunst <jbkunst@gmail.com>
2017-09-25 01:38:00 -0300

    megaupload

--- R/setup.R
+++ R/setup.R
@@ -8,7 +8,55 @@ library(stringr)
 
 knitr::opts_chunk$set(message = FALSE, warning = FALSE, echo = TRUE)
 options(highcharter.theme = hc_theme_smpl(chart = list(backgroundColor = "transparent")))
+
+
+# ggplot ------------------------------------------------------------------
 theme_set(theme_jbk(base_family = NA, plot_margin = margin(10, 10, 10, 10)))
 
+update_geom_defaults("line",  list(colour = "#03a9f4", size = 1.05))
+update_geom_defaults("point", list(colour = "#03a9f4", size = 3))
+update_geom_defaults("bar",   list(fill = "#03a9f4"))
+update_geom_defaults("text",  list(size = 4, colour = "#666666"))
+
 # helpers -----------------------------------------------------------------
-R <- function() {  as.character(tags$span("R", style = "color:#2066B9;font-weight:500")) }
+sw <- function(w, f) {
+  # file.exists(f) break
+  htmlwidgets::saveWidget(widget = w, file = f, libdir = "index_files/", selfcontained = FALSE, background = "transparent")
+}
+
+# vis-dist ----------------------------------------------------------------
+df <- data_frame(
+  name = sample(head(LETTERS, 5), size = 200, replace = TRUE, prob = (1:5)^(1.2))
+) %>% 
+  count(name) %>% 
+  rename(y = n) %>% 
+  mutate(color = colorize(name, rev(viridis(5))))
+df
+
+set.seed(123)
+
+x <- c(rgamma(6000, 5, 5), rnorm(3000, 3, 1)) %>% 
+  abs() %>% 
+  { .[. < quantile(., .999)]}
+
+hc <- hchart(x, color = "#21908C") %>% 
+  hc_add_series(
+    data = df, type = "pie", center = c('70%', '50%'), size = 400,
+    dataLabels = list(enabled = FALSE), 
+    innerSize =  '60%', borderWidth = 0
+  ) %>% 
+  hc_plotOptions(
+    series = list(showInLegend = FALSE)
+  ) %>% 
+  hc_add_theme(
+    hc_theme_merge(
+      hc_theme_null(), hc_theme_flatdark()
+    )
+  ) 
+
+hc
+  
+sw(hc, "vis-dis.html")
+
+
+
