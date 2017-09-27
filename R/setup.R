d3e62f09f278fdc16f0104a4821a1820fd87013b
Joshua Kunst <jbkunst@gmail.com>
2017-09-27 01:33:00 -0300

    updating index

--- R/setup.R
+++ R/setup.R
@@ -26,41 +26,41 @@ update_geom_defaults("bar",   list(fill = main_color))
 update_geom_defaults("text",  list(size = 4, colour = "#666666"))
 
 # helpers -----------------------------------------------------------------
-sw <- function(w, f) {
-  # file.exists(f) break
-  htmlwidgets::saveWidget(widget = w, file = f, libdir = "index_files/", selfcontained = FALSE, background = "transparent")
-}
+# sw <- function(w, f) {
+#   # file.exists(f) break
+#   htmlwidgets::saveWidget(widget = w, file = f, libdir = "index_files/", selfcontained = FALSE, background = "transparent")
+# }
 
 # vis-dist ----------------------------------------------------------------
-df <- data_frame(
-  name = sample(head(LETTERS, 5), size = 200, replace = TRUE, prob = (1:5)^(1.2))
-) %>% 
-  count(name) %>% 
-  rename(y = n) %>% 
-  mutate(color = colorize(name, rev(viridis(5))))
-df
-
-set.seed(123)
-
-x <- c(rgamma(6000, 5, 5), rnorm(3000, 3, 1)) %>% 
-  abs() %>% 
-  { .[. < quantile(., .999)]}
-
-hc <- hchart(x, color = "#21908C") %>% 
-  hc_add_series(
-    data = df, type = "pie", center = c('70%', '50%'), size = 350,
-    dataLabels = list(enabled = FALSE), 
-    innerSize =  '60%', borderWidth = 0
-  ) %>% 
-  hc_plotOptions(
-    series = list(showInLegend = FALSE)
-  ) %>% 
-  hc_add_theme(
-    hc_theme_merge(
-      hc_theme_null(), hc_theme_flatdark()
-    )
-  ) 
-
-hc
-  
-sw(hc, "vis-dis.html")
+# df <- data_frame(
+#   name = sample(head(LETTERS, 5), size = 200, replace = TRUE, prob = (1:5)^(1.2))
+# ) %>% 
+#   count(name) %>% 
+#   rename(y = n) %>% 
+#   mutate(color = colorize(name, rev(viridis(5))))
+# df
+# 
+# set.seed(123)
+# 
+# x <- c(rgamma(6000, 5, 5), rnorm(3000, 3, 1)) %>% 
+#   abs() %>% 
+#   { .[. < quantile(., .999)]}
+# 
+# hc <- hchart(x, color = "#21908C") %>% 
+#   hc_add_series(
+#     data = df, type = "pie", center = c('70%', '50%'), size = 350,
+#     dataLabels = list(enabled = FALSE), 
+#     innerSize =  '60%', borderWidth = 0
+#   ) %>% 
+#   hc_plotOptions(
+#     series = list(showInLegend = FALSE)
+#   ) %>% 
+#   hc_add_theme(
+#     hc_theme_merge(
+#       hc_theme_null(), hc_theme_flatdark()
+#     )
+#   ) 
+# 
+# hc
+#   
+# sw(hc, "vis-dis.html")
