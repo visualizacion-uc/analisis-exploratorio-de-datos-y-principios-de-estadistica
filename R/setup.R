0aa119a01936d9898a65df158bdd9ac80300fdb4
Joshua Kunst <jbkunst@gmail.com>
2017-09-27 03:17:27 -0300

    81%

--- R/setup.R
+++ R/setup.R
@@ -25,6 +25,23 @@ update_geom_defaults("point", list(colour = main_color, size = 3))
 update_geom_defaults("bar",   list(fill = main_color))
 update_geom_defaults("text",  list(size = 4, colour = "#666666"))
 
+theme_null <- function(...) {
+  theme(...,
+        axis.line=element_blank(),
+        axis.text.x=element_blank(),
+        axis.text.y=element_blank(),
+        axis.ticks=element_blank(),
+        axis.title.x=element_blank(),
+        axis.title.y=element_blank(),
+        legend.position = "none",
+        panel.background=element_blank(),
+        panel.border=element_blank(),
+        panel.grid.major=element_blank(),
+        panel.grid.minor=element_blank(),
+        plot.background=element_blank())
+}
+
+
 # helpers -----------------------------------------------------------------
 # sw <- function(w, f) {
 #   # file.exists(f) break
